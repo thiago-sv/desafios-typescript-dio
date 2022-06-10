3746c8eb20132b970e1313e92ec9a85ac9ea00f3
thiago-sv <floriponario@gmail.com>
2022-06-10 19:50:10 -0300

    Segundo commit

---
+++ src/respostasdesafios/desafio1.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const funcionario = {
+    codigo: 10,
+    nome: 'João'
+};
+
+const funcionario2 = {
+    codigo: 10,
+    nome: 'joao'
+};
+;
+const funcionarioObj = {};
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+const funcionarioObj2 = {
+    codigo: 10,
+    nome: 'João'
+};
